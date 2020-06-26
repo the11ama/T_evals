cd5890f022cf19c0681dc4d5316681fa1b60c568
the11ama <the11ama@users.noreply.github.com>
2020-06-26 18:05:19 -0400

    T4
    Not much here

---
+++ T4/T4/target/classes/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE UserRepository (
+	
+);
+
+CREATE TABLE FlashcardRepository (
+	
+);
+
+CREATE TABLE User (
+	
+);
+
+CREATE TABLE Flashcard (
+	
+);
+
+CREATE TABLE Role (
+	
+);
+
+CREATE TABLE Category (
+	
+);
+
+CREATE TABLE AppDriver (
+	
+);
